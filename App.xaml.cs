138496b0c47516a60aaeadbacf0c6ac0e4b7f9e1
GrzegorzRomanczyk <baczekexp@gmail.com>
2022-10-22 14:14:13 +0200

    RelayCommand and some setups

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using XGameXShop.View;
+using XGameXShop.ViewModels;
 
 namespace XGameXShop
 {
@@ -13,12 +16,35 @@ namespace XGameXShop
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            MainWindow = new MainWindow();
-            MainWindow.Show();
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    MainWindow = new MainWindow();
+        //    MainWindow.Show();
 
-            base.OnStartup(e);
+        //    base.OnStartup(e);
+        //}
+
+        //public App()
+        //{
+        //}
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            IContainer container = Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                MainWindow mainWindow = scope.Resolve<MainWindow>();
+                mainWindow.Show();
+            }
+        }
+
+        public IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ViewModelMainWindow>().SingleInstance();
+            builder.RegisterType<MainWindow>();
+            builder.RegisterType<NewProductWindow>();
+            return builder.Build();
         }
     }
 }
