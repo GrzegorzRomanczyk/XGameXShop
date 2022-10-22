d949701db4d7fcfa55044b4457af4fb33020c15a
GrzegorzRomanczyk <baczekexp@gmail.com>
2022-10-23 01:00:33 +0200

    enum converter

---
+++ Converters/IConsoleTypConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGameXShop.Converters
+{
+    interface IConsoleTypConverter
+    {
+        string ConvertEnumToSting(ConsoleEnum consoleType);
+    }
+}
